50026e633f2e9ebcc276959780ee2c1387c8512e
Shixing Mao <mao.shixi@northeastern.edu>
2025-07-22 19:18:38 -0700

    A3 JAVASCRIPT

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -3,10 +3,8 @@ import { ListGroup } from "react-bootstrap";
 import ModuleControlButtons from "./ModuleControlButtons";
 import { BsGripVertical } from "react-icons/bs";
 import LessonControlButtons from "./LessonControlButtons";
-import { useParams } from "react-router";
 import * as db from "../../Database";
 export default function Modules() {
-  const { cid } = useParams();
   const modules = db.modules;
 
   return (
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -31,7 +31,6 @@ import VariableTypes from "./VariableTypes";
 import Square from "./Square";
 import Highlight from "./Highlight";
 import PathParameters from "./PathParameters";
-import AddPathParameters from "./AddPathParameters";
 
 export default function Lab3() {
   console.log('Hello World!');
