b8d8e8dc08ae7e5f48c16b1939af5f83e3b86902
Shixing Mao <mao.shixi@northeastern.edu>
2025-08-17 17:26:39 -0700

    Project

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { setCurrentUser } from "./reducer";
 import { useDispatch } from "react-redux";
-import { Button, FormControl } from "react-bootstrap";
+import { Button, Card, FormControl, Nav } from "react-bootstrap";
 import * as client from "./client";
 
 export default function Signin() {
@@ -28,6 +28,38 @@ export default function Signin() {
         className="mb-2" placeholder="password" type="password" id="wd-password" />
       <Button onClick={signin} id="wd-signin-btn" className="w-100" > Sign in </Button>
       <Link id="wd-signup-link" to="/Kambaz/Account/Signup"> Sign up </Link>
+
+      <Card>
+        <Card.Header>Team & Project Info</Card.Header>
+        <Card.Body>
+          <h6 className="mb-2">Team Member</h6>
+          <ul className="mb-3">
+              <li>
+                <strong>Shixing Mao</strong>
+              </li>
+          </ul>
+
+          <h6 className="mb-2">Links</h6>
+          <Nav className="flex-column">
+            <Nav.Item>
+              <Nav.Link href="https://github.com/ShixingMao/web-dev/" target="_blank" rel="noreferrer">
+                Frontend GitHub
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="https://github.com/ShixingMao/kambaz-node-server-app" target="_blank" rel="noreferrer">
+                Server GitHub
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="https://kambaz-node-server-app-project-jcse.onrender.com" target="_blank" rel="noreferrer">
+                Render Server
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Body>
+      </Card>
+      
     </div>
   );
 }
